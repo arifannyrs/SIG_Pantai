63efbe414f4ad2fad1ff249bc6232f4b98be890e
Arifannyrs <arifannyrs@gmail.com>
2021-05-04 06:46:01 +0700

    koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "sig_pantai";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+$base_url = "http://localhost:8080/SIG_Pantai1/";
