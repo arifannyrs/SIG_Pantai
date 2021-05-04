08e387c1bc021d867fe79f3674d9f2319cacc643
Arifannyrs <arifannyrs@gmail.com>
2021-05-05 04:37:29 +0700

    Backend proses daftar user

--- proses.php
+++ proses.php
@@ -51,3 +51,58 @@ if(isset($_POST["login"])){
         return false;
     }
 }
+
+// Proses daftar user
+
+//cek jika tombol register di klik
+//maka jalankan script ini.
+if(isset($_POST["register"])){
+    //cek value data yg dikirimkan dari form login, jika salah satu ada yg kosong
+    if($_POST["email"] =="" | $_POST["username"] == "" | $_POST["password"] == ""){
+       //maka tampilkan notif dan lalu redirect kembali ke halaman register.
+        echo'<script>
+                alert("Jangan ada yang kosong !");
+                windows.location="'.$base_url.'/register.php";
+            </script>';
+        return false;//hentikan proses sampai sini.
+    }
+
+    //Jika semua value data dari form register sudah benar atau salah satu tidak ada yang kosong
+    //maka jalankan script ini.
+    $email = $_POST["email"]; // $email yg di isi dng value email dari form register
+    $username = $_POST["username"]; // $username yg di isi dng value username dari form register
+    $password = md5($_POST["password"]); // $password yg di isi dng value password dari form register
+   
+    //cek adakah email si pengguna di table login.
+    $cek = mysqli_query($conn,"SELECT * FROM login WHERE email='$email'");
+    //jika hasil 1 artinya email si pengguna register sudah ada di table login
+    if(mysqli_num_rows($cek) === 1){
+       //maka tampilkan notif ini
+       echo'<script>
+                 alert("Email ini sudah terdaftar, silahkan coba dengan email lain !");
+                 window.location="'.$base_url.'/register.php";
+             </script>';
+         return false; //hentikan proses sampai sini.
+    }
+ 
+    //jika email si pengguna register tidak ada dlm table login
+    //maka jalankan aksi penyimpanan data baru ke table login.
+    $save = mysqli_query($conn,"INSERT INTO login(username,email,password) VALUES('$username', '$email', '$password')");
+    //cek proses penyimpanan berhasil atau tidak
+    //jika true artinya berhasil
+    if ($save === true) {
+         //maka tampilkan notif ini
+         //lalu redirect ke halaman index.php atau halaman login.
+         echo'<script>
+                 alert("Registrasi Berhasil...");
+                 window.location="'.$base_url.'/index.php"; 
+              </script>';
+    }else{
+          //jika proses gagal, tampilkan notif ini lalu redirect kembali ke halaman register.
+          echo'<script>
+                 alert("Registrasi Gagal !");
+                 window.location="'.$base_url.'/register.php";
+              </script>';
+         return false;
+    }
+}
