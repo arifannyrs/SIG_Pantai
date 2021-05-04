29bc863d22298482cf8b80c44563749ae65ca76d
Arifannyrs <arifannyrs@gmail.com>
2021-05-05 04:19:43 +0700

    Front end dashboard admin

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<?php
+  session_start();
+  include("../koneksi.php");
+  include("../proses.php");
+
+  if(!isset($_SESSION["username"])){
+      echo'<script>
+                alert("Mohon login dahulu !");
+                window.location="../index.php";
+             </script>';
+      return false;
+  }
+
+  if($_SESSION["level"] != "admin"){
+        echo'<script>
+                alert("Maaf Anda Tidak Berhak Ke Halaman ini !");
+                window.location="../'.$_SESSION["level"].'/";
+             </script>';
+        return false;
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>SIG PANTAI</title>
+      <link rel="stylesheet" type="text/css" href="../style.css">
+  </head>
+  <body>
+      <p align="center"><a href="<?=$base_url;?>/logout.php" class="tombol_keluar">KELUAR</a></p>           
+  </body>
+</html>
