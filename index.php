a47fd0eac329c9566a670a8d1eafc95ef67f630f
Arifannyrs <arifannyrs@gmail.com>
2021-05-04 07:01:50 +0700

    Frontend login

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SIG PANTAI</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+        
+    <p><center>
+			<img src='img/logokab.png' width='120' height='140'></a>
+    </center>
+            
+	<h1>Sistem Informasi Geografis Pemetaan Potensi<br/> Objek Wisata Pantai di Kabupaten Tulungagung</h1>
+ 
+		<div class="kotak_login">
+			<p class="tulisan_login">Silahkan login</p>
+            <form action="" method="post">
+				<label>Username</label>
+				<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+ 
+  				<label>Password</label>
+	  			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+
+                <button type="submit" name="login" class="tombol_login">LOGIN</button>
+            </form>
+        </div>
+      </div>
+	</div>
+  </body>
+</html>
