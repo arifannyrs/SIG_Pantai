3944be55c445e9df192d1b2b8c83c30b6d1e33c8
Arifannyrs <arifannyrs@gmail.com>
2021-05-22 00:46:12 +0700

    Update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,22 @@
 <?php
+	// connection
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db   = 'sig_pantai';
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "sig_pantai";
+	$koneksi = mysqli_connect($host,$user,$pass);
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+	if (!$koneksi)
+	{
+		echo "Tidak dapat terkoneksi dengan server";
+		exit();
+	}
 
-$base_url = "http://localhost:8080/SIG_Pantai/";
+	if(!mysqli_select_db($koneksi, $db))
+	{
+		echo "Tidak dapat menemukan database";
+		exit();
+	}
+    $base_url = "http://localhost:8080/sig_pantai/";
+?>
